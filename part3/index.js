c51f462110b5bb0d94573512070d458d948d6211
unknown <duongquockhang.1205@gmail.com>
2023-06-10 19:31:27 +0300

    Task 3.6 done

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-
-const phonebookEntries = [
+app.use(express.json());
+const { v4: uuidv4 } = require("uuid");
+let phonebookEntries = [
   {
     id: 1,
     name: "Arto Hermanie",
@@ -38,7 +39,45 @@ app.get("/info", (req, res) => {
     </div>
   `);
 });
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const entry = phonebookEntries.find((entry) => entry.id === id);
 
+  if (entry) {
+    res.json(entry);
+  } else {
+    res.status(404).json({ error: "Phone number not found" });
+  }
+});
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const updatedPhonebookEntries = phonebookEntries.filter(
+    (entry) => entry.id !== id
+  );
+  phonebookEntries = updatedPhonebookEntries;
+  res.status(204).end();
+});
+app.post("/api/persons", (req, res) => {
+  const { name, number } = req.body;
+  if (!name || !number) {
+    return res.status(400).json({ error: "Name or number is missing" });
+  }
+  const existingEntry = phonebookEntries.find((entry) => entry.name === name);
+  if (existingEntry) {
+    return res
+      .status(409)
+      .json({ error: "Name already exists in the phonebook" });
+  }
+  const newEntry = {
+    id: uuidv4(),
+    name,
+    number,
+  };
+
+  phonebookEntries.push(newEntry);
+
+  res.status(201).json(newEntry);
+});
 // Start the server
 const PORT = 3001;
 app.listen(PORT, () => {
