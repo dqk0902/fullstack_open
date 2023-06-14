1f3f58c791ac0fe01e9fa2d69f820ff76ef8bf90
unknown <duongquockhang.1205@gmail.com>
2023-06-15 00:56:24 +0300

    Task 3.9 done

--- part3/index.js
+++ part3/index.js
@@ -90,7 +90,7 @@ app.post("/api/persons", (req, res) => {
   res.status(201).json(newEntry);
 });
 // Start the server
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
